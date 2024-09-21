7a2ab53e1b85f5e8b0a6f990f0708dbfbe8a52a0
SiawYs <shinance@hotmail.co.uk>
2024-09-21 10:13:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,30 @@
-# function to check string is
-# palindrome or not
-def isPalindrome(str):
+def example_function  (x,y):
+     if x>10:
+      print("X is greater than 10")
+  else:
+          print(   "X is 10 or less")
+  for i in range( 0,10):
+     if  i% 2==0:print(    f"{i} is even")
+     else:
+      print(f"{i} is odd") 
+      return  x+y
+def   another_function( a,  b ):
+          if a == b:
+       print("a is equal to b") ; print("This is a sample statement");
+               return None
+ else:
+  print(    "a is not equal to b");return a -b
 
-    # Run loop from 0 to len/2
-    for i in range(0, int(len(str)/2)):
-        if str[i] != str[len(str)-i-1]:
-            return False
-    return True
+    class SampleClass :
+ def  __init__( self, value ) :
+           self.value=value
+          def display_value(  self  ):
+             print(  "The value is",self.value)
 
-# main function
-s = "malayalam"
-ans = isPalindrome(s)
-
-if (ans):
-    print("Yes")
-else:
-    print("No")
+def  main( ) :
+      ex_result=example_function(10,20)
+ print("Result of example_function:", ex_result)
+         obj=SampleClass(   42)
+    obj.display_value()
+another_function(5,10)
+main(   )
