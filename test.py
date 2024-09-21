5856f1ad97ccc0c7c5759becd15e3f53c6cc83af
SiawYs <shinance@hotmail.co.uk>
2024-09-21 10:09:28 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,18 @@
-# function which return reverse of a string
+# function to check string is
+# palindrome or not
+def isPalindrome(str):
 
-def isPalindrome(s):
-  return s == s[::-1]
+    # Run loop from 0 to len/2
+    for i in range(0, int(len(str)/2)):
+        if str[i] != str[len(str)-i-1]:
+            return False
+    return True
 
-
-# Driver code
+# main function
 s = "malayalam"
 ans = isPalindrome(s)
 
-if ans:
+if (ans):
     print("Yes")
 else:
     print("No")
