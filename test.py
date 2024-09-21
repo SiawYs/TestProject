385db6aba63ed4c7e42ecc5ac761d84e7beafd27
SiawYs <shinance@hotmail.co.uk>
2024-09-21 10:24:41 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,39 +1,20 @@
-import math
-import sys
+def is_unique(
+			s
+			):
+	s = list(s
+				)
+	s.sort()
 
-def calc_circle_area(radius):
-    pi = 3.14159
 
-    if radius < 0:
-        return "Error: Negative radius not allowed"
-    area = pi * (radius**2)
+	for i in range(len(s) - 1):
+		if s[i] == s[i + 1]:
+			return 0
+	else:
+		return 1
 
-    return area
 
-class Shape:
-    def __init__(self, name):
-        self.name = name
-
-    def display_name(self):
-        print(f"Shape name is: {self.name}")
-
-def complicated_function(a, b, c):
-    if a + b > c:
-        print("a + b is greater than c")
-        return a * b
-    elif a + b < c:
-        print("a + b is less than c")
-        return a - b
-    else:
-        print("a + b is equal to c")
-        return 0
-
-def main_function():
-    shape = Shape("Circle")
-    shape.display_name()
-    result = calc_circle_area(5)
-    print("Area of circle:", result)
-
-    complicated_function(10, 5, 20)
-
-main_function()
+if __name__ == "__main__":
+	print(
+		is_unique(input())
+		)
+    
